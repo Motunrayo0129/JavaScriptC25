5322d46dd2aa32c1d0c3debf9a1379e34f689896
Motunrayo0129 <onibudomotunrayo2901@gmail.com>
2025-06-17 17:28:28 +0100

    firedrill

---
+++ PhaseGate/task_five.js
@@ -0,0 +1,8 @@
+	for (let num = 1; num <= 10; num++)  {
+	for (let number = 1; number <= 5; number++) { 
+		if (num % 4 == 0){
+		
+console.log(num + " ");
+		}
+	}
+}
---
+++ PhaseGate/task_one.py
@@ -0,0 +1,4 @@
+for(int num = 1; num <= 10; num++)  {
+		console.log(num + " ");
+}
+   
